20274e4c22e74f391017eae387a981d5e6e3a396
vittalbee650 <vittalbee650@gmail.com>
2025-04-09 14:55:20 +0000

    Add 27.rs

---
+++ src/27.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let first_name = String::from("Alice");
+    let last_name = String::from("Doe");
+
+    println!("First name: {}", first_name);
+    println!("Last name: {}", last_name);
+}
